da94e0ce8b77cb85ef9fdc231f18be7a6fb58657
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2021-08-07 17:04:54 +0400

    lesson30

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,20 +1,28 @@
   const sendForm = () => {
     const errorMessage = 'Что-то пошло не так',
       loadMassage = 'Загрузка...',
-      succsessMessage = 'Спасибо! Скоро мы с вами свяжемся!';
-    const form1 = document.getElementById('form1'),
+      succsessMessage = 'Спасибо! Скоро мы с вами свяжемся!',
+      form1 = document.getElementById('form1'),
       form2 = document.getElementById('form2'),
       form3 = document.getElementById('form3'),
-      input = document.querySelectorAll('input');
-    const statusMessage = document.createElement('div'),
-      img = document.createElement('img');
+      formEmail1 = document.getElementById('form1-email'),
+      formEmail2 = document.getElementById('form2-email'),
+      formEmail3 = document.getElementById('form3-email'),
+      formPhone1 = document.getElementById('form1-phone'),
+      formPhone2 = document.getElementById('form2-phone'),
+      formPhone3 = document.getElementById('form3-phone'),
+      input = document.querySelectorAll('input'),
+      statusMessage = document.createElement('div'),
+      img = document.createElement('img'),
+      regExpEmail = /\S+@\S+\.\S{2}/,
+      regExpTel = /.{16}/;
 
     statusMessage.style.cssText = `
     font-size: 2rem;
     color: white;`;
 
     const postData = (body) => {
-      return fetch('./server1.php', {
+      return fetch('./server.php', {
         method: 'POST',
         body: JSON.stringify(body)
       });
@@ -76,16 +84,41 @@
         .catch(error => errorFunc(error));
     };
     form1.addEventListener('submit', (event) => {
+      console.log(formPhone1.value);
       event.preventDefault();
-      formFunc(form1);
+      if (regExpEmail.test(formEmail1.value)) {
+        if (regExpTel.test(formPhone1.value)) {
+          formFunc(form1);
+        } else {
+          alert('введите валидный номер телефона');
+        }
+      } else {
+        alert('введите валидный e-mail');
+      }
     });
     form2.addEventListener('submit', (event) => {
       event.preventDefault();
-      formFunc(form2);
+      if (regExpEmail.test(formEmail2.value)) {
+        if (regExpTel.test(formPhone2.value)) {
+          formFunc(form2);
+        } else {
+          alert('введите валидный номер телефона');
+        }
+      } else {
+        alert('введите валидный e-mail');
+      }
     });
     form3.addEventListener('submit', (event) => {
       event.preventDefault();
-      formFunc(form3);
+      if (regExpEmail.test(formEmail3.value)) {
+        if (regExpTel.test(formPhone3.value)) {
+          formFunc(form3);
+        } else {
+          alert('введите номер телефона до конца');
+        }
+      } else {
+        alert('введите валидный e-mail');
+      }
     });
 
   };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,21 +4,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname ,'./dist')
+    path: path.resolve(__dirname, './dist')
   },
   mode: 'production',
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env']
-          },
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
         },
-        exclude: /node_modules/,
-      }
-    ]
+      },
+      exclude: /node_modules/,
+    }]
   }
 };
